614e7e7ef27e72aeb636bfb9429b28d555f1e554
Oscar <oticonaf@unjbg.edu.pe>
2024-06-13 00:04:25 -0500

    Agrega ejercicio 1 de Estructuras Repetitivas

---
+++ Ejercicio_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,i,m;
+	cout<<"Programa que imprimira los primeros n multiplos de 7"<<endl;
+	cout<<"Ingrese el valor de n"<<endl;
+	cin>>n;
+	for(i=1;i<=n;i++) {
+		m=i*7;
+		cout<<m<<endl;
+	}
+	return 0;
+}
