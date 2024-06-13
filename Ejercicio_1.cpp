b7087d55ea85da15d7c320097387f45830218495
Oscar <oticonaf@unjbg.edu.pe>
2024-06-13 00:21:45 -0500

    Actualiza ejercicio 1 de Estructuras Repetitivas: Agrega validacion de entrada

--- Ejercicio_1.cpp
+++ Ejercicio_1.cpp
@@ -6,6 +6,10 @@ int main()
 	cout<<"Programa que imprimira los primeros n multiplos de 7"<<endl;
 	cout<<"Ingrese el valor de n"<<endl;
 	cin>>n;
+	while (n<0) {
+				cout<<"*Invalido! Introduzca un numero positivo"<<endl;
+				cin>>n;
+			}
 	for(i=1;i<=n;i++) {
 		m=i*7;
 		cout<<m<<endl;
